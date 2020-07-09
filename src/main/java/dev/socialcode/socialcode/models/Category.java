56db4221f3532db04898b184da309f89614c9ebb
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-09 12:15:26 -0500

    sample Category model

--- src/main/java/dev/socialcode/socialcode/models/Category.java
+++ src/main/java/dev/socialcode/socialcode/models/Category.java
@@ -1,4 +1,19 @@
 package dev.socialcode.socialcode.models;
 
+@Entity
+@Table(name="categories")
 public class Category {
-}
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false)
+        private String name;
+
+        @ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY)
+        @JsonIgnore
+        private List<Category> categories;
+
+        //add setters and getters
+    }

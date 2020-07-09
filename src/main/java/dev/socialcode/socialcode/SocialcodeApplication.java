585d732f4ed05d156985663173b6ce02e160243f
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-09 10:26:33 -0500

    first commit

---
+++ src/main/java/dev/socialcode/socialcode/SocialcodeApplication.java
@@ -0,0 +1,13 @@
+package dev.socialcode.socialcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/socialcode/socialcode/SocialcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.socialcode.socialcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

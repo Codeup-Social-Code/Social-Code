284c80a2526fa388ba184bb1c931813502496811
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-09 14:33:56 -0500

    sample User model

--- src/main/java/dev/socialcode/socialcode/models/User.java
+++ src/main/java/dev/socialcode/socialcode/models/User.java
@@ -1,4 +1,38 @@
 package dev.socialcode.socialcode.models;
 
+@Entity
+@Table(name = "users")
 public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(nullable = false, length = 20)
+        @NotBlank(message = "Username can't be empty")
+        private String firstName;
+
+        @Column(nullable = false, length = 20)
+        @NotBlank(message = "Username can't be empty")
+        private String lastName;
+
+        @Column(nullable = false, unique = true)
+        @Email(message = "Invalid email")
+        @NotBlank(message = "Email can't be empty")
+        private String email;
+
+        @Column(nullable = false)
+        @NotBlank(message = "Password can't be empty")
+        @JsonIgnore
+        private String password;
+
+        @OneToMany(mappedBy = "user")
+        @JsonBackReference
+        private List<Post> posts;
+
+        @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+        private List<Comment> comments;
+
+
+
 }

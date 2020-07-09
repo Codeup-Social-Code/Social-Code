1fcfe2283d98cc2adba1d1e319e1aad9c7992b04
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-09 12:20:55 -0500

    Comment sample model

--- src/main/java/dev/socialcode/socialcode/models/Comment.java
+++ src/main/java/dev/socialcode/socialcode/models/Comment.java
@@ -1,4 +1,40 @@
 package dev.socialcode.socialcode.models;
 
+@Entity
+@Table(name = "comments")
 public class Comment {
-}
+
+        @Autowired
+        @Transient
+        private CommentRepository commentRepository;
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column
+        private String body;
+
+        @ManyToOne
+        private User user;
+
+        //the parent references what?.....
+        @ManyToOne
+        private Comment parent;
+
+        @ManyToOne
+        private Post post;
+
+        @CreationTimestamp
+        @Temporal(TemporalType.TIMESTAMP)
+        @Column()
+        private Date createdAt;
+
+        //what is the parent/children relationship here??
+        @OneToMany(mappedBy = "parent")
+        private List<Comment> children;
+
+
+        //add getters and setters
+
+    }

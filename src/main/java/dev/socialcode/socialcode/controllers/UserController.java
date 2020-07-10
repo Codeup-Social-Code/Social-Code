0aff64915a4e60db20b7c4a79ba493d0eeb956c3
SUNG IL LEE <sungil.lee90@gmail.com>
2020-07-09 22:28:55 -0500

    commiting before restart IntelliJ, having problem with 'could not autowir' No beans of PasswordEncoder was found.

--- src/main/java/dev/socialcode/socialcode/controllers/UserController.java
+++ src/main/java/dev/socialcode/socialcode/controllers/UserController.java
@@ -1,8 +1,35 @@
 package dev.socialcode.socialcode.controllers;
 
+import dev.socialcode.socialcode.daos.UserRepository;
+import dev.socialcode.socialcode.models.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private UserRepository users;
+    private PasswordEncoder passwordEncoder;
 
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
 }

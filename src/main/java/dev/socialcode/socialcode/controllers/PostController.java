513c3686be121ea943372153fa674bca8e404ff3
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-07-10 10:26:42 -0500

    feat: initial setup for creat-post form returning string for now.

--- src/main/java/dev/socialcode/socialcode/controllers/PostController.java
+++ src/main/java/dev/socialcode/socialcode/controllers/PostController.java
@@ -1,12 +1,31 @@
 package dev.socialcode.socialcode.controllers;
 
+import dev.socialcode.socialcode.daos.PostsRepository;
+import dev.socialcode.socialcode.models.Post;
+import net.bytebuddy.asm.Advice;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class PostController {
 
+    private PostsRepository postsDao;
+
+    public PostController(PostsRepository postsRepository) {
+        this.postsDao = postsRepository;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String showForm(Model viewModel) {
+        viewModel.addAttribute("post", new Post());
+        return "This wil be the create form";
+    }
 
 
 }
--- src/main/java/dev/socialcode/socialcode/daos/PostsRepository.java
+++ src/main/java/dev/socialcode/socialcode/daos/PostsRepository.java
@@ -1,4 +1,9 @@
 package dev.socialcode.socialcode.daos;
 
-public interface PostsRepository {
+import dev.socialcode.socialcode.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Post, Long> {
+
+
 }

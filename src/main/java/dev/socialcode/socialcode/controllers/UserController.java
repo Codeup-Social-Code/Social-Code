2d5518a75cf63a3974dad76839e729164a3c2904
SUNG IL LEE <sungil.lee90@gmail.com>
2020-07-11 23:55:51 -0500

    feat: duplication checker added. But still need to test it.

--- src/main/java/dev/socialcode/socialcode/controllers/UserController.java
+++ src/main/java/dev/socialcode/socialcode/controllers/UserController.java
@@ -5,6 +5,7 @@ import dev.socialcode.socialcode.models.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,11 @@ public class UserController {
     }
 
     @PostMapping("/sign-up")
-    public String saveUser(@ModelAttribute User user){
+    public String saveUser(@ModelAttribute User user, Errors validation){
+        User existingEmail = users.findByEmail(user.getEmail());
+        if(existingEmail != null) {
+            validation.rejectValue("email", "user.email", "Duplicated email " + user.getEmail());
+        }
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         users.save(user);

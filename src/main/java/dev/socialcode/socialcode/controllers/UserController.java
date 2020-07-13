b3b2a92ee183f5c86fbbf990927599a5c0671eae
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-13 10:20:08 -0500

    added notes about revisions/questions to controller

--- src/main/java/dev/socialcode/socialcode/controllers/UserController.java
+++ src/main/java/dev/socialcode/socialcode/controllers/UserController.java
@@ -21,26 +21,34 @@ public class UserController {
     }
 
     @GetMapping("/sign-up")
-    public String showSignupForm(Model model){
+    public String showSignupForm(Model model) {
         model.addAttribute("user", new User());
         return "users/sign-up";
     }
 
     @PostMapping("/sign-up")
-    public String saveUser(@ModelAttribute User user){
+    public String saveUser(@ModelAttribute User user) {
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         usersDao.save(user);
         return "redirect:/login";
+        //redirects go to urls, not to files
     }
-}
 
     //get single user view
     @GetMapping("/users/{id}")
-    public String getUser (@PathVariable long id, Model model) {
+    public String getUser(@PathVariable long id, Model model) {
         User user = usersDao.getOne(id);
         model.addAttribute("id", id);
         model.addAttribute("user", user);
         return "users/user";
 
+
+        //also I changed the UserRepository instance from users
+        // to usersDao for clarity
+
+        //what other controllers do we need?
+
     }
+
+}

ea9573978cf4428c6c89c8e01208d49facb9e161
SUNG IL LEE <sungil.lee90@gmail.com>
2020-07-16 11:18:38 -0500

    init setup for using sendgrid

--- src/main/java/dev/socialcode/socialcode/services/EmailService.java
+++ src/main/java/dev/socialcode/socialcode/services/EmailService.java
@@ -1,4 +1,34 @@
 package dev.socialcode.socialcode.services;
 
+import dev.socialcode.socialcode.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService") // annotation tells Spring that this is a class that will be managed by spring.
 public class EmailService {
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void prepareAndSend(User user, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(user.getUsername()); // userName = email
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
 }

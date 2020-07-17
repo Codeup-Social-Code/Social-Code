ed045b1bb80662db778c7f9e739c0906bfbe38be
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-17 13:03:20 -0500

    added temporary menu to navbar

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -28,12 +28,12 @@ let slideInterval = setInterval(nextSlide, 2100);
 
 // which slide to move to as denoted by "s"
 const goToSlide = (s) => {
-  /* 
+  /*
    when this function is run, hide all slides
    since the slide class has opacity set to 0.
   */
     slides[currentSlide].className = 'slide';
-    
+
      /* assuming we have 4 different slides
         we can index these slides as follows:
         Slide 1 = 0
@@ -45,7 +45,7 @@ const goToSlide = (s) => {
 
         currentSlide = 0
 
-        Now, we need to find a way to increment the currentSlide index 
+        Now, we need to find a way to increment the currentSlide index
         every 2.1 seconds as specified in our setInterval method.
 
         recall the slide.length = 4
@@ -53,18 +53,18 @@ const goToSlide = (s) => {
 
         currentSlide = 0 + 4/4 = 1 [new currentSlide]
 
-        now that the currentSlide is 1, when the setInterval runs 
+        now that the currentSlide is 1, when the setInterval runs
         it again, we have:
 
         currentSlide = 1 + 4/4 = 2 [new currentSlide]
 
-        now that the currentSlide is 2, when the setInterval runs 
+        now that the currentSlide is 2, when the setInterval runs
         it again, we have:
 
         currentSlide = 2 + 4/4 = 3 [new currentSlide]
 
         since our index stops at 3. The setInternal starts from the
-        beginning again by assigning 0 to currentSlide and doing same 
+        beginning again by assigning 0 to currentSlide and doing same
         process all over again.
 
      */
@@ -80,7 +80,7 @@ let pauseButton = document.querySelector("#pause");
 const pauseSlideshow = () => {
     // when slideshow is paused show the play icon
     pauseButton.innerHTML = '<i class="fas fa-play-circle"></i>';
-    // when pause we set an interim variable to indicate not playing 
+    // when pause we set an interim variable to indicate not playing
     playing = false;
     // stop the sideshow on pause
     clearInterval(slideInterval);
@@ -88,8 +88,8 @@ const pauseSlideshow = () => {
 
 const playSlideshow = () => {
     // while playing show the pause icon
-    pauseButton.innerHTML = '<i class="fas fa-pause-circle"></i>'; 
-    // when playing we set an interim variable to indicate playing 
+    pauseButton.innerHTML = '<i class="fas fa-pause-circle"></i>';
+    // when playing we set an interim variable to indicate playing
     playing = true;
     // start a slideshow on play
     slideInterval = setInterval(nextSlide, 2100);
@@ -104,7 +104,7 @@ let next_btn = document.querySelector('#next');
 let previous_btn = document.querySelector('#previous');
 
 next_btn.onclick = () => {
-   /* 
+   /*
     when the next button is clicked do two things:
       1. run the pauseSLideshow function
       2. run the nextSlide function (adding 1 to the currentSlide)
@@ -113,7 +113,7 @@ next_btn.onclick = () => {
     nextSlide();
 };
 previous_btn.onclick = () => {
-  /* 
+  /*
    when the next button is clicked do two things:
      1. run the pauseSLideshow function
      2. run the previousSlide function (removing 1 from the currentSlide)

dee388c367ab95bc3f42d02161edfe28958a41ce
DeLayne LaBove <delayne.labove@gmail.com>
2020-07-17 10:23:28 -0500

    Feature: Fixed create view to accept event_start and event_date

--- src/main/java/dev/socialcode/socialcode/daos/PostRepository.java
+++ src/main/java/dev/socialcode/socialcode/daos/PostRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findTop9ByOrderByIdDesc();
+//    List<Post> findTop9ByOrderByIdDesc();
 
 
 }

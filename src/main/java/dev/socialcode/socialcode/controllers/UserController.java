10bc5fca9142021f4cd8969a368841af7991fff6
DeLayne LaBove <delayne.labove@gmail.com>
2020-07-18 10:41:48 -0500

    Grid

--- src/main/java/dev/socialcode/socialcode/controllers/UserController.java
+++ src/main/java/dev/socialcode/socialcode/controllers/UserController.java
@@ -9,12 +9,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
@@ -79,6 +75,7 @@ public class UserController {
 
         return "users/user";
     }
+
 
     @GetMapping("/users/profile")
     public String showProfile(Model viewModel){

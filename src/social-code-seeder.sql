25c8540d1a8b77021e9d7133d927334f8937e823
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-07-21 13:35:07 -0500

    feat: added seeder for all tables.

--- src/social-code-seeder.sql
+++ src/social-code-seeder.sql
@@ -35,3 +35,29 @@ VALUES ('Fourth Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit
 INSERT INTO posts (title, body, create_date, event_end, event_start, event_time)
 VALUES ('Fifth Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aspernatur dolor exercitationem nemo quia quis quo saepe. Ad aspernatur, autem beatae dolorem impedit labore nesciunt non officia, sed sequi veniam!
 ', curdate(), '07/07/2020', '07/07/2020', '12:00');
+
+
+INSERT INTO comments (body, post_id, user_id) VALUE('I want to join!.', 1, 1);
+INSERT INTO comments (body, post_id, user_id) VALUE('I want to join!.', 2, 1);
+INSERT INTO comments (body, post_id, user_id) VALUE('I want to join!.', 1, 2);
+INSERT INTO comments (body, post_id, user_id) VALUE('I want to join!.', 3, 1);
+INSERT INTO comments (body, post_id, user_id) VALUE('I want to join!.', 4, 1);
+
+INSERT INTO categories(id, name) VALUES (1, 'Java');
+INSERT INTO categories(id, name) VALUES (2, 'JavaScript');
+INSERT INTO categories(id, name) VALUES (3, 'Bootstrap');
+INSERT INTO categories(id, name) VALUES (4, 'CSS');
+INSERT INTO categories(id, name) VALUES (5, 'Frontend');
+INSERT INTO categories(id, name) VALUES (6, 'Backend');
+INSERT INTO categories(id, name) VALUES (7, 'Spring');
+INSERT INTO categories(id, name) VALUES (9, 'TechLife');
+
+INSERT INTO rsvp VALUES (1, 1);
+INSERT INTO rsvp VALUES (2, 2);
+INSERT INTO rsvp VALUES (1, 2);
+INSERT INTO rsvp VALUES (3, 1);
+INSERT INTO rsvp VALUES (4, 1);
+INSERT INTO rsvp VALUES (2, 1);
+INSERT INTO rsvp VALUES (1, 3);
+INSERT INTO rsvp VALUES (2, 2);
+INSERT INTO rsvp VALUES (3, 3);

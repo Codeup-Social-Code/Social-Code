ce9c7b60f4bd3306d57d400f541cc91bd7b7ffd4
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-07-23 12:53:13 -0500

    feat: added seeder for categories.

---
+++ src/categories-seeder.sql
@@ -0,0 +1,17 @@
+USE socialcode_db;
+
+INSERT INTO categories(id, name) VALUES (1, 'Javascript');
+INSERT INTO categories(id, name) VALUES (2, 'Java');
+INSERT INTO categories(id, name) VALUES (3, 'Bootstrap');
+INSERT INTO categories(id, name) VALUES (4, 'CSS');
+INSERT INTO categories(id, name) VALUES (5, 'HTML');
+INSERT INTO categories(id, name) VALUES (6, 'MySQL');
+INSERT INTO categories(id, name) VALUES (7, 'JQuery');
+INSERT INTO categories(id, name) VALUES (8, 'Spring');
+INSERT INTO categories(id, name) VALUES (9, 'UX Design');
+INSERT INTO categories(id, name) VALUES (10, 'Front-end');
+INSERT INTO categories(id, name) VALUES (11, 'Back-end');
+INSERT INTO categories(id, name) VALUES (13, 'Github');
+INSERT INTO categories(id, name) VALUES (14, 'Code Review');
+INSERT INTO categories(id, name) VALUES (15, 'Tech Life');
+INSERT INTO categories(id, name) VALUES (16, 'Other');

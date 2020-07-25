88af484cb9ddf7d3b4a8d3e02810a9fee94b1dbe
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-25 10:40:55 -0500

    created first tests

--- src/test/java/dev/socialcode/socialcode/SocialCodeIntegrationTests.java
+++ src/test/java/dev/socialcode/socialcode/SocialCodeIntegrationTests.java
@@ -5,6 +5,7 @@ import dev.socialcode.socialcode.daos.PostRepository;
 import dev.socialcode.socialcode.daos.UserRepository;
 import dev.socialcode.socialcode.models.User;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.HttpSession;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -67,6 +69,17 @@ public class SocialCodeIntegrationTests {
                 .getSession();
     }
 
+    @Test
+    public void contextLoads() {
+        // Sanity Test, just to make sure the MVC bean is working
+        assertNotNull(mvc);
+    }
+
+    @Test
+    public void testIfUserSessionIsActive() throws Exception {
+        // It makes sure the returned session is not null
+        assertNotNull(httpSession);
+    }
 
 
 }
